7fd28f8a0949e545f3a72c116a1b979e389e4fee
unknown <kinomata1@.(none)>
2009-12-19 21:47:33 +0900

    part1

---
+++ NodeCollection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace AutoLayoutApplication
+{
+    public class NodeCollection : Dictionary<string, Node>
+    {
+        public NodeCollection Add(params string[] names)
+        {
+            foreach (string name in names)
+            {
+                this[name] = new Node(name);
+            }
+            return this;
+        }
+
+        public NodeCollection Link(params string[] links)
+        {
+            foreach (string link in links)
+            {
+                string[] n = link.Split(',');
+                this[n[0]].Neighbors.Add(this[n[1]]);
+                this[n[1]].Neighbors.Add(this[n[0]]);
+            }
+            return this;
+        }
+
+        public void MoveAll()
+        {
+            const double dt = 0.1d;
+            foreach (Node n in this.Values)
+            {
+                Vector f = new Vector();
+                foreach (Node nn in n.Neighbors)
+                {
+                    f += n.GetSpringForce(nn);
+                }
+                foreach (Node nn in this.Values)
+                {
+                    if (n != nn)
+                    {
+                        f += n.GetReplusiveForce(nn);
+                    }
+                }
+                f += n.GetFrictionalForce();
+                n.MoveEular(dt, f);
+            }
+        }
+
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AutoLayoutApplication
+{
+    public struct Vector
+    {
+        public double X;
+        public double Y;
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector()
+            {
+                X = v1.X + v2.X,
+                Y = v1.Y + v2.Y
+            };
+        }
+    }
+}
